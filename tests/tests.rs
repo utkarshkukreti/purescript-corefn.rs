ac45a62c7be7ac50a36a3f13244a4887012909ef
Utkarsh Kukreti <utkarshkukreti@gmail.com>
2018-09-09 20:16:38 +0530

    feat: better output when test fails

--- tests/tests.rs
+++ tests/tests.rs
@@ -4,5 +4,5 @@ extern crate purescript_corefn;
 fn t() {
     assert!(purescript_corefn::from_str("{}").is_err());
     let str = include_str!("./Main/corefn.json");
-    assert!(purescript_corefn::from_str(str).is_ok());
+    println!("{:?}", purescript_corefn::from_str(str).unwrap());
 }
